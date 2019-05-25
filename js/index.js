90180100daaf42f6b03394c9a8fba5047e94f94d
Josef Benninghoff <jsbenning4@gmail.com>
2019-05-25 22:21:35 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,68 @@
 // your code here
+f// This first version is without parsing JSON
+// function showRepositories() {
+//   console.log(this.responseText);
+//   let repoList = '<ul>';
+//   for (var i = 0; i < this.responseText.length; i++) {
+//     repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+//   }
+//   repoList += '</ul>';
+//   document.getElementById('repositories').innerHTML = repoList;
+// }
+
+
+// This 2nd version parses JSON titles, returns them as li elements
+// function showRepositories() {
+//   var repos = JSON.parse(this.responseText);
+//   console.log(repos);
+//   const repoList = `<ul>${repos
+//     .map(r => '<li>' + r.name + '</li>')
+//     .join('')}</ul>`;
+//   document.getElementById('repositories').innerHTML = repoList;
+// }
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
+
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
